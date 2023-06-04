f5df404bd9c1962d2b1c3ce1995e5f1e928ea051
chhatley2 <107590817+chhatley2@users.noreply.github.com>
2023-06-04 14:46:37 -0500

    Create initial directory

--- main.tf
+++ main.tf
@@ -1,1 +1,47 @@
+terraform {
+ required_providers {
+  azurerm = {
+  source  = "hashicorp/azurerm"
+  version = "3.43.0"
+  }
+ }
+ backend "azurerm" {
+  resource_group_name  = "RG-ALZ-DEVOPS-PROD-001"
+  storage_account_name = "terrastate001"
+  container_name       = "terraform"
+  key                  = "${var.sub_name}.tfstate"
+ }
+}
 
+provider "azurerm" {
+ # Configuration options
+ features {}
+}
+
+data "azurerm_client_config" "current" {
+}
+
+########################################################
+## New Subscription's Virtual Network ##################
+########################################################
+module "network" {
+    source = "./modules/virtualnetwork"
+    
+    name = "st${sub_name}tstate01"
+    location = var.location
+    resource_group_name = "RG-${sub_name}-VNET-001"
+    address_space = [var.address_space]
+    dns_servers = [var.dns_servers]
+    subnet_name
+}
+
+########################################################
+## New Subscription's Terraform State Stroage Account ##
+########################################################
+module "terrafrom_storage" {
+    source = "./modules/storage"
+    
+    name = "st${sub_name}tstate01"
+    location = var.location
+    resource_group_name = "RG-${sub_name}-MGMT-001"
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,36 @@
+#################################
+# Global Configuration
+#################################
+
+variable "sub_name" {
+  description = "Friendly Name of the New Subscription. No more than 8 characters. All CAPS"
+  type        = string
+  default     = "SANDBOX"
+}
+
+variable "location" {
+  description = "Default location of deployment. GAO's primary data center is EastUS. GAO's secondary data center is WestUS."
+  type        = string
+  default     = "EastUS"
+}
+
+variable "address_space" {
+  description = "Address space of the entire vNet that will support subnet resources."
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+variable "dns_servers" {
+  description = "Default DNS server for vNet. This should point to Azure-hosted domain controllers."
+  type        = list(string)
+  default     = ["172.18.3.4"]
+}
+
+variable "mgmt_subnet_address_prefix" {
+  description = "Address prefix for the default management subnet. ."
+  type        = string
+  default     = "10.0.0.0/24"
+}
+
+
 
